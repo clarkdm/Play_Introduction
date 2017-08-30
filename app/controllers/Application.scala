e147aadb0838fd0bfeaf2c04bc67e86f4a387e5b
clarkdm <dmclark@hotmail.co.uk>
2017-08-30 17:09:08 +0100

    CDs Form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,31 +1,35 @@
 package controllers
 
+import javax.inject.Inject
+
+
 import play.api._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  def index = Action { implicitly: Request[AnyContent] =>
+  def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def index_Cookie(cookie: String = "hi") = Action { implicitly: Request[AnyContent] =>
-    Ok(views.html.index("Your new application is ready.")).withCookies(Cookie("test", cookie))
+  def index_Cookie(cookie: String = "hi") = Action { implicit request =>
+    Ok(views.html.index("Your new application is ready.")).withCookies(Cookie("test", cookie)).withSession("connected" -> "user@gmail.com")
   }
 
 
-  def goo = Action { implicitly: Request[AnyContent] =>
+  def goo = Action { implicit request =>
     Redirect("https://www.google.co.uk")
   }
 
-  def web(page: String) = Action { implicitly: Request[AnyContent] =>
+  def web(page: String) = Action { implicit request =>
     Redirect("https://www." + page + ".com")
   }
 
 
-  def helpers(result: String) = Action { implicitly: Request[AnyContent] =>
+  def helpers(result: String) = Action { implicit request =>
     result match {
-      case "OK" => Ok(views.html.status("OK"))
+      case "OK" => Ok(views.html.status("OK" + request.session.get("connected").head))
       case "BadGateway" => BadGateway(views.html.status(result))
       case "GatewayTimeout" => GatewayTimeout(views.html.status(result))
       case "Created" => Created(views.html.status(result))
@@ -35,12 +39,39 @@ class Application extends Controller {
 
   }
 
+  def todo = TODO
 
-  def cookies = Action { implicitly: Request[AnyContent] =>
 
-    Ok(views.html.status(implicitly.cookies.get("test").head.value.toString()))//.discardingCookies(DiscardingCookie("test"))
+  def cookies = Action { implicit request =>
+
+    Ok(views.html.status(request.cookies.get("test").head.value.toString()))
 
   }
+
+  def no_cookies = Action { implicit request =>
+
+    Ok(views.html.status(request.cookies.get("test").head.value.toString())).discardingCookies(DiscardingCookie("test"))
+  }
+
+  def listcd = Action { implicit request =>
+    Ok(views.html.listCDs(CD.cds, CD.createCDForm))
+
+  }
+
+  def addcd = Action { implicit request =>
+
+    val formValidationRerult = CD.createCDForm.bindFromRequest
+
+    formValidationRerult.fold({
+      formWithErrors =>
+        BadRequest(views.html.listCDs(CD.cds, formWithErrors))
+    }, { cd =>
+      cd
+      CD.cds.append(cd)
+      Redirect(routes.Application.listcd())
+    })
+  }
+
 }
 
 
---
+++ app/controllers/CD.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.data._
+import play.api.data.Forms._
+import scala.collection.mutable.ArrayBuffer
+
+case class CD(name: String, price: Int)
+
+object CD {
+  val createCDForm = Form(mapping("name" -> nonEmptyText, "price" -> number(min = 0, max = 222))(CD.apply)(CD.unapply))
+  val cds = ArrayBuffer(CD("cd 1", 43), CD("cd 2", 122), CD("cd 3", 533))
+}
