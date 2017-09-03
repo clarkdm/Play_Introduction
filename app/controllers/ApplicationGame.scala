0bb5baff7b2b2aff4c6c07ee5efb70cd20e436f4
clarkdm <dmclark@hotmail.co.uk>
2017-09-03 18:16:01 +0100

    MongoDB add

--- app/controllers/ApplicationGame.scala
+++ app/controllers/ApplicationGame.scala
@@ -1,58 +1,174 @@
 package controllers
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Controller, Cookie}
 import javax.inject.Inject
 
-import models.{Game, Login, User}
-import play.api._
-import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc._
+import scala.concurrent.{Await, Future}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import reactivemongo.api.Cursor
+import models._
+import models.Formats._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.play.json._
+import collection._
 
-class ApplicationGame @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+class ApplicationGame @Inject()(val messagesApi: MessagesApi, val reactiveMongoApi: ReactiveMongoApi)
+  extends Controller with I18nSupport with MongoController with ReactiveMongoComponents {
+
+  def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("games"))
 
 
-  def games = Action { implicit request =>
-    Ok(views.html.GamesList(Game.games))
+  def createUser = Action.async {
+    val user = User("john", "jmith", "hi")
+
+    // insert the user
+    val futureResult = collection.flatMap(_.insert(user))
+
+    // when the insert is performed, send a OK 200 result
+    futureResult.map(_ => Ok)
   }
+
+  def createGame(game: Game) = {
+
+
+    // insert the user
+    //    val futureResult = collection.flatMap(_.insert(Game(3,"Dragon Age",44,"hi")))
+    val futureResult = collection.flatMap(_.insert(game))
+
+    // when the insert is performed, send a OK 200 result
+
+  }
+
+
+  def findByUserName(userName: String) = Action.async {
+    // let's do our query
+    val cursor: Future[Cursor[User]] = collection.map {
+      // find all people with name `name`
+      //      _.find(Json.obj("userName" -> lastName)).
+      _.find(Json.obj("userName" -> userName)).
+        // perform the query and get a cursor of JsObject
+        cursor[User]
+    }
+    println(cursor)
+    // gather all the JsObjects in a list
+    val futureUsersList: Future[List[User]] = cursor.flatMap(_.collect[List]())
+
+    // everything's ok! Let's reply with the array
+    futureUsersList.map { persons =>
+      Ok(persons.toString)
+    }
+  }
+
+//  def findByAllGames = {
+//    // let's do our query
+//
+//    //    var x: List[Game] = List()
+//
+//
+//    val cursor: Future[Cursor[Game]] = collection.map {
+//      _.find(Json.obj()).cursor[Game]
+//    }
+////
+////
+//    val futureUsersList: Future[List[Game]] = cursor.flatMap(_.collect[List]())
+//    futureUsersList.onComplete(result => Ok(result.get))
+//    // everything's ok! Let's reply with the array
+//    //    futureUsersList.map { games =>
+//    ////      println(games)
+//    //      x = games
+//    //return games
+//    //    }
+//    //    println(x)
+//    //x
+//  }
+
+
+
+
+
+  def games: Action[AnyContent] = Action.async { implicit request =>
+    val cursor: Future[Cursor[Game]] = collection.map {
+      //_.find(Json.obj("lastName" -> "Lastname"))  // searching by a particular field
+      _.find(Json.obj())                            // getting averything from the collection
+        //.sort(Json.obj("id" -> -1))
+        .cursor[Game]
+    }
+    val futureUsersList: Future[List[Game]] = cursor.flatMap(_.collect[List]())
+    futureUsersList.map { persons =>
+      Ok(views.html.GamesList(persons))
+    }
+  }
+
+
+
+
+
+//  def games = Action { implicit request =>
+//    findByAllGames
+//    val cursor: Future[Cursor[Game]] = collection.map {
+//      _.find(Json.obj()).cursor[Game]
+//    }
+//
+//
+//    val futureUsersList: Future[List[Game]] = cursor.flatMap(_.collect[List]())
+//    futureUsersList.onComplete(result => Ok(views.html.GamesList(result.get)))
+//
+//
+//
+//Ok("sssss")
+//  }
 
   def addGames = Action { implicit request =>
 
-    Ok(views.html.AddGame(Game.createGameForm))
+    Ok(views.html.AddGame(Formats.createGameForm))
 
   }
+
   def addGameToCart = Action { implicit request =>
 
-    Ok(views.html.GamesList(Game.games))
-
+    //Ok(views.html.GamesList(findByAllGames))
+    var x: ListBuffer[Game] = ListBuffer()
+    
+    Ok(views.html.GamesList(x.toList))
   }
 
   def addGamePost = Action { implicit request =>
 
-    val formValidationRerult = Game.createGameForm.bindFromRequest
+    val formValidationRerult = Formats.createGameForm.bindFromRequest
 
     formValidationRerult.fold({
       formWithErrors =>
         BadRequest(views.html.AddGame(formWithErrors))
     }, { game =>
-
-      Game.games.append(game)
+      createGame(game)
+      //Game.games.append(game)
       Redirect(routes.ApplicationGame.addGames)
     })
   }
+
   def register = Action { implicit request =>
 
-    Ok(views.html.register(User.createUserForm))
+    Ok(views.html.register(Formats.createUserForm))
 
   }
 
   def addUser = Action { implicit request =>
 
-    val formValidationRerult = User.createUserForm.bindFromRequest
+    val formValidationRerult = Formats.createUserForm.bindFromRequest
 
     formValidationRerult.fold({
       formWithErrors =>
         BadRequest(views.html.register(formWithErrors))
     }, { user =>
 
-      User.users.append(user)
+      //User.users.append(user)
       Redirect(routes.ApplicationGame.register)
     })
   }
@@ -65,21 +181,25 @@ class ApplicationGame @Inject()(val messagesApi: MessagesApi) extends Controller
       formWithErrors =>
         BadRequest(views.html.Login_P(formWithErrors))
     }, { login =>
-println(login+" #############################"+(login.user_name=="cd1")+" : "+(login.password=="1234")+"###################")
-      if(login.user_name=="cd1"&&login.password=="1234"){
-        Redirect(routes.ApplicationGame.games).withSession("login"-> login.user_name)
-      } else Redirect(routes.ApplicationGame.games).withSession("login"-> "")
+      println(login + " #############################" + (login.user_name == "cd1") + " : " + (login.password == "1234") + "###################")
+      if (login.user_name == "cd1" && login.password == "1234") {
+        Redirect(routes.ApplicationGame.games).withSession("login" -> login.user_name)
+      } else Redirect(routes.ApplicationGame.games).withSession("login" -> "")
       //User.users.append(login)
 
     })
   }
 
-def addtocart(game: String) = Action { implicit request =>
+  def addtocart(game: String) = Action { implicit request =>
 
-  request.session.get("cart")=="hi"
-if (request.cookies.get("cart").isEmpty){Ok("hi").withCookies(Cookie("cart", game))}
-  else {Ok("hicccc").withCookies(Cookie("cart", (request.cookies.get("cart").head.value+"###"+game)))}
+    request.session.get("cart") == "hi"
+    if (request.cookies.get("cart").isEmpty) {
+      Ok("hi").withCookies(Cookie("cart", game))
+    }
+    else {
+      Ok("hicccc").withCookies(Cookie("cart", (request.cookies.get("cart").head.value + "###" + game)))
+    }
 
-}
+  }
 
 }
--- app/models/Game.scala
+++ app/models/Game.scala
@@ -1,18 +1,18 @@
-package models
-
-import play.api.data.Forms._
-import play.api.data._
-
-import scala.collection.mutable.ArrayBuffer
-
-case class Game(name: String, price: Int,description:String)
-object Game {
-  val createGameForm = Form(mapping("name" -> nonEmptyText, "price" -> number(min = 0, max = 222),"description" -> text) (Game.apply)(Game.unapply))
-
-
-  val games = ArrayBuffer(
-    Game("Overwatch", 46,"The world needs heroes. Join over 30 million players* as you clash on the battlefields of tomorrow. Choose your hero from a diverse cast of soldiers, scientists, adventurers, and oddities. Bend time, defy physics, and unleash an array of extraordinary powers and weapons. Engage your enemies in iconic locations from around the globe in the ultimate team-based shooter. Take your place in Overwatch."),
-    Game("Mass_Effect", 27,"Mass Effect: Andromeda takes players to the Andromeda galaxy, far beyond the Milky Way. There, players will lead our fight for a new home in hostile territory as the Pathfinder - a leader of military-trained explorers. This is the story of humanity’s next chapter, and player choices throughout the game will ultimately determine our survival."),
-    Game("For_Honor", 24,"Carve a path of destruction through the battlefield in For Honor, a brand-new game developed by the renowned Ubisoft Montreal studio."),
-    Game("For_Honor", 24,"Carve a path of destruction through the battlefield in For Honor, a brand-new game developed by the renowned Ubisoft Montreal studio."))
-}
+//package models
+//
+//import play.api.data.Forms._
+//import play.api.data._
+//
+//import scala.collection.mutable.ArrayBuffer
+//
+//case class Game(id: Int, name: String, price: Int, description: String)
+//
+//object Game {
+//
+//
+//  val games = ArrayBuffer(
+//    Game(1, "Overwatch", 46, "The world needs heroes. Join over 30 million players* as you clash on the battlefields of tomorrow. Choose your hero from a diverse cast of soldiers, scientists, adventurers, and oddities. Bend time, defy physics, and unleash an array of extraordinary powers and weapons. Engage your enemies in iconic locations from around the globe in the ultimate team-based shooter. Take your place in Overwatch."),
+//    Game(2, "Mass Effect", 27, "Mass Effect: Andromeda takes players to the Andromeda galaxy, far beyond the Milky Way. There, players will lead our fight for a new home in hostile territory as the Pathfinder - a leader of military-trained explorers. This is the story of humanity’s next chapter, and player choices throughout the game will ultimately determine our survival."),
+//    Game(3, "For Honor", 24, "Carve a path of destruction through the battlefield in For Honor, a brand-new game developed by the renowned Ubisoft Montreal studio."),
+//    Game(4, "For Honor", 24, "Carve a path of destruction through the battlefield in For Honor, a brand-new game developed by the renowned Ubisoft Montreal studio."))
+//}
